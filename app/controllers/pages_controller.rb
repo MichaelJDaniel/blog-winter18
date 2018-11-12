a0991ed3d1460f474ac309b131728a5a64622d77
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-12 14:29:18 -0700

    add changes to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,42 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
   end
 
-  def create
+  def new
+    @page = Page.new
+  end
+  
+  def create 
+    @page = Page.new(pages_params)
+    @page = Page.new
+    
+
+    if (@page.save)
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  def edit 
+    @page = Page.find(params[:id])
+  end
+
+  def update 
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+    redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+    def destroy
+      @page = Page.find(params[:id])
+
+      
+  private 
+  def pages_params
+    params.require(:page).permit(:title, :body, :author, :active)
   end
 end
